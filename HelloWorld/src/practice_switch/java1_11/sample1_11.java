a6461001e6c05e6dac000529f7d1141511e2666f
hamaguchi madoka <maaaaaa2001.32032.do@gmail.com>
2023-03-31 22:44:03 +0900

    1.11 switch型条件分岐

---
+++ HelloWorld/src/practice_switch/java1_11/sample1_11.java
@@ -0,0 +1,54 @@
+/*
+	問題．switch文を使用し、以下のプログラムを作成してください。
+			変数に数値を代入
+			
+		①2の場合は、「（変数）月は28日です。」
+			出力結果：2月は28日です。
+			
+		②4，6，9，11の場合は、「（変数）月は30日です。」
+			出力例：4月は30日です。
+			
+		③その他の場合は、「（変数）月は31日です。」
+			出力例：1月は31日です。
+*/
+package practice_switch.java1_11;
+
+public class sample1_11 {
+
+	public static void main(String[] args) {
+		int num = 2;
+		switch (num) {
+		case 2:
+			System.out.println(num + "月は28日です。");
+			break;
+		}
+		int num1 = 4;
+		switch (num1) {
+		case 4:
+			System.out.println(num1 + "月は30日です。");
+			break;
+		case 6:
+			System.out.println(num1 + "月は30日です。");
+			break;
+		case 9:
+			System.out.println(num1 + "月は30日です。");
+			break;
+		case 11:
+			System.out.println(num1 + "月は30日です。");
+			break;
+		
+		}
+		int num2=1;
+		switch(num2) {
+		case 5:
+			System.out.println(num2+"月は31日です。");
+			break;
+		case 7:
+			System.out.println(num2+"月は31日です。");
+			break;
+		default:
+			System.out.println(num2+"月は31日です。");
+			break;
+		}
+	}
+}
