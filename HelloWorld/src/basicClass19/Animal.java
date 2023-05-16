f93f8c97a294633858c0ba9e934908d916a5c41a
hamaguchi madoka <maaaaaa2001.32032.do@gmail.com>
2023-05-16 15:42:10 +0900

    課題１９の修正

--- HelloWorld/src/basicClass19/Animal.java
+++ HelloWorld/src/basicClass19/Animal.java
@@ -4,10 +4,8 @@ import java.util.Date;
 
 public class Animal {
 	public static void main(String[] args) {
-
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		Dog dog = new Dog(); // Dog　classを呼び出してる
-
 		System.out.println(dog.name); // dog classの名前を出力
 
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
--- HelloWorld/src/basicClass19/Dog.java
+++ HelloWorld/src/basicClass19/Dog.java
@@ -2,19 +2,17 @@ package basicClass19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	String name;
+	String name; // 名前を定義
 	
 	// Q2：フィールドに動物の数の変数を定義してください。
-	int num;
+	int num; // 数の定義
 	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	public Dog() {
-		this.name = "犬";
-	}
+		this.name = "犬"; // 犬を代入
 	
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	public Dog(int num) {
-		this.num = num;
+		this.num = 6; // 6を代入
 	}
 }
 
